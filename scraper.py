d378ea65b358778505905ec3fb416411eb90e14c
NoBadVibes79 <krazerog@outlook.com>
2024-04-30 22:26:45 +0300

    added scraper class

--- scraper.py
+++ scraper.py
@@ -4,6 +4,19 @@ from pyrogram.types import Message
 
 from config import API_ID, API_HASH
 
-async def parser_content(donor_channel_id:int, my_channel_id:int) -> AsyncGenerator[Message, None]:
-    async with Client('my_bot', api_id=API_ID, api_hash=API_HASH) as app:
-        pass
+
+class Scraper:
+    def __init__(self, client:Client, test_flag:bool=True) -> None:
+        if test_flag:
+            self.client = Client('test_bot', api_id=API_ID, api_hash=API_HASH)
+        else:
+            self.client =  client
+    async def parser_content(donor_channel_id:int, my_channel_id:int) -> AsyncGenerator[Message, None]:
+        async with Client('my_bot', api_id=API_ID, api_hash=API_HASH) as app:
+            pass
+        
+
+if __name__ == '__main__':
+    
+    
+    scraper = Scraper(client=)
