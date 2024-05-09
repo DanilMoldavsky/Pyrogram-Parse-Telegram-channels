61e8701aeabf75720a8765225518d6db605ff362
NoBadVibes79 <krazerog@outlook.com>
2024-05-09 21:39:51 +0300

    parse members complete

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, idle
 from typing import AsyncGenerator
 from pyrogram.types import Message, List
+from pyrogram.raw.functions.channels import GetFullChannel
 
 import asyncio
 import logging
@@ -14,7 +15,7 @@ class Scraper:
     #     else:
     #         self.client =  client
             
-    async def parser_content_open(
+    async def parser_content_open_example(
             self, 
             donor_channel_id: int | str,
             limit_mess:int=50, 
@@ -29,7 +30,8 @@ class Scraper:
             for message in reversed_messages:
                 await message.copy(chat_id=target_channel_id)
 
-    async def parser_content_closed(
+
+    async def parser_content_closed_example(
             self, 
             donor_channel_id: int | str,
             limit_mess:int=50, 
@@ -40,7 +42,7 @@ class Scraper:
             messages: AsyncGenerator[Message, None] = app.get_chat_history(chat_id=donor_channel_id, limit=limit_mess)
             
             reversed_messages: List[Message] = await self._reversed_messages(messages)
-            
+
             for message in reversed_messages:
                 if message.video:
                     video = await message.download(in_memory=True)
@@ -58,6 +60,20 @@ class Scraper:
                     await app.send_message(chat_id=target_channel_id, text=message.text)
 
 
+    async def parser_members(
+            self, 
+            donor_channel_id: int | str,
+            limit_mess:int=50, 
+            target_channel_id:str=config.MY_CHANNEL_ID
+        ) -> AsyncGenerator[Message, None]:
+        
+        async with Client("test_aiassist_bot", api_id=config.API_ID_TEST, api_hash=config.API_HASH_TEST) as app:
+            members: AsyncGenerator[Message, None] = app.get_chat_members(chat_id=donor_channel_id, limit=limit_mess)
+        
+            with open('members.txt', 'w') as f:
+                async for member in members:
+                    f.write(f'{member.user.first_name} {member.user.last_name} {member.user.username} {member.user.id}\n')
+
 
     async def _reversed_messages(self, messages: AsyncGenerator[Message, None]) -> AsyncGenerator[Message, None]:
         reverse_message = [message async for message in messages]
@@ -69,4 +85,5 @@ if __name__ == '__main__':
         format="%(asctime)s - %(levelname)s - %(message)s",
         )
     scraper = Scraper()
-    asyncio.run(scraper.parser_content_closed(donor_channel_id=-1001983581219, limit_mess=3))# vgtimes id = -1001338358512
+    # asyncio.run(scraper.parser_content_closed(donor_channel_id=-1001983581219, limit_mess=3))# vgtimes id = -1001338358512
+    asyncio.run(scraper.parser_members(donor_channel_id=-1001983581219, limit_mess=3))# vgtimes id = -1001338358512
